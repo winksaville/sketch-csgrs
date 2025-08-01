08701f355542124d5ac91e84ae891acb4b6647e2
Wink Saville <wink@saville.com>
2025-07-31 20:35:56 -0700

    feat: Add command line parameters

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,36 @@
 //! sketch-csgrs
+use clap::Parser;
 use csgrs::{float_types::Real, sketch::Sketch};
+
+#[derive(Parser, Debug)]
+#[clap(name = env!("CARGO_PKG_NAME"), version = env!("CARGO_PKG_VERSION"), author = env!("CARGO_PKG_AUTHORS"), about = env!("CARGO_PKG_DESCRIPTION"))]
+struct Args {
+    width: Real,
+    height: Real,
+
+    #[arg(
+        short,
+        long,
+        default_value = "false",
+        help = "Enable printing Mesh"
+    )]
+    print_mesh: bool,
+}
 
 fn main() {
     println!("sketch-csgrs:+");
 
+    // Parse command line arguments
+    let args = Args::parse();
+
     // Create a square
-    let width = Real::NAN;
-    let height = 10.0;
+    let width: Real = args.width;
+    let height = args.height;
     let square_sketch: Sketch<()> = Sketch::square(width, None);
     let square = square_sketch.extrude(height);
-    println!("Generated mesh: {square:?}");
+    if args.print_mesh {
+        println!("square Mesh: {square:?}");
+    }
 
     // Save the mesh as an STL file
     let name = "extrude_square";
